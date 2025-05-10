409a5882267fc50f0ed32c264815df5c582b0a22
gaurav <gauravbhongade95@gmail.com>
2025-05-10 18:16:24 +0530

    db settings changed

--- wahooart_clone/settings.py
+++ wahooart_clone/settings.py
@@ -63,9 +63,17 @@ WSGI_APPLICATION = 'wahooart_clone.wsgi.application'
 # Otherwise, fall back to SQLite
 
 DATABASES = {
-        'default': {
+        'Sqlt': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+        },
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': 'postgres',
+            'PASSWORD': 'XjumetxEolhbdUPbImUCXvqRbPFoKOhr',
+            'HOST': 'postgres.railway.internal',
+            'PORT': '5432'
         }
     }
 
