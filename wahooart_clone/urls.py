b7da64986d94c7f2712788505521e6f2330f7edb
gaurav-bhongade <gauravbhongade95@gmail.com>
2025-08-04 17:19:45 +0530

    unwanted stuff removed

--- wahooart_clone/urls.py
+++ wahooart_clone/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('artshop.urls')),
     path('', include('customizaion_api.urls')),
     path('registration_api/', include('registrationapi.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
